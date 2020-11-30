e4fe39c6ad080b59024de6a4ebcc4e3914f2a3b3
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2020-11-30 17:02:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function setDate() {
-    document.getElementById('date').innerHTML = new Date().toDateString();
+function author() {
+    window.alert("Pilkey was born in Cleveland, Ohio on March 4, 1966, to Barbara and David Pilkey Sr. He has one older sister. Pilkey was diagnosed with attention deficit hyperactivity disorder and dyslexia as a child. In elementary school in North Ridgeville, Ohio, Pilkey was frequently reprimanded for his behavior in class and thus usually sat at a desk in the school hallway, where he created the Captain Underpants character. Pilkey entered his first book, World War Won, in a national competition for student authors and won in his age category. The book's publication in 1987 was included in the award.");
 }
 
-setDate();
+
